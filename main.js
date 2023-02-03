0935e5ac1e8238eea5087ce65e047d81f9311dc8
David Wolf <68775926+devidw@users.noreply.github.com>
2023-02-03 13:48:42 +0100

    add error handling

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+import * as dotenv from 'dotenv'
 import { WebSocket } from "ws"
 import { writeFileSync } from "fs"
+
+dotenv.config()
+
+const REQUIRED_ENV_VARS = [
+  "REPLIKA_CHAT_ID",
+  // "REPLIKA_TOKEN",
+  "REPLIKA_USER_ID",
+  "REPLIKA_AUTH_TOKEN",
+  "REPLIKA_DEVICE_ID",
+]
+
+REQUIRED_ENV_VARS.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`Missing environment variable: ${envVar}`)
+  }
+})
 
 const ws = new WebSocket("wss://ws.replika.com/v17")
 
@@ -9,7 +26,8 @@ const query = {
     chat_id: process.env.REPLIKA_CHAT_ID,
     limit: 100,
   },
-  token: process.env.REPLIKA_TOKEN,
+  // token: process.env.REPLIKA_TOKEN,
+  token: process.env.REPLIKA_AUTH_TOKEN,
   auth: {
     user_id: process.env.REPLIKA_USER_ID,
     auth_token: process.env.REPLIKA_AUTH_TOKEN,
@@ -25,16 +43,23 @@ ws.on("open", () => {
   ws.on("message", (data) => {
     const json = JSON.parse(data.toString())
 
+    if (!json.payload || !json.payload.messages) {
+      console.error("Unexpected message", json)
+      process.exit(1)
+    }
+
     const newMessages = json.payload.messages
 
     if (newMessages.length > 0) {
       messages = [...newMessages, ...messages]
-      console.log(`${newMessages.length} new messages, total: ${messages.length}`)
+      console.log(
+        `${newMessages.length} new messages, total: ${messages.length}`
+      )
       const newQuery = {
         ...query,
         payload: {
           ...query.payload,
-          last_message_id: newMessages[0].id
+          last_message_id: newMessages[0].id,
         },
       }
       ws.send(JSON.stringify(newQuery))
